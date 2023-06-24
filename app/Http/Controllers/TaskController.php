8834dc27f558c2ccdbf492a765946ecccd7854a5
TijsVsN <132552284+TijsVsN@users.noreply.github.com>
2023-06-24 19:20:29 +0200

    File storage + unzipping of the .dphx
    I worked way to long on this

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rules\File;
 
@@ -36,17 +36,27 @@ class TaskController extends Controller
             'max_time' => 'required',
         ]);
 
-        // //Validates the DPHX (Broken for now)
-        // Validator::validate($request, [
-        //     'dphx_file' => [ 
-        //         'required',
-        //         File::types(['dphx'])
-        //     ],
-        // ]);
-
-        //Save the DPHX
+        //Store the DPHX
         $dphx_file = $request->file('dphx_file');
+        $dphx_name = $dphx_file->getClientOriginalName();
+        // $dphx_ext = $dphx_file->getClientOriginalExtension();
+        $dphx_name_without_ext = $request->input('dphx_file');
 
+        Storage::disk('public')->putFileAs('dphx',$dphx_file, $dphx_name);
+
+        if(Storage::disk('public')->exists('dphx', $dphx_name)) {
+            $dphx_zip = Storage::disk('public')->path('dphx/'.$dphx_name);
+            $extractTo = pathinfo($dphx_zip, PATHINFO_FILENAME); // Extract the file name without extension
+            $extractPath = Storage::disk('public')->path('dphx/'.$extractTo);
+
+            $zip = new ZipArchive();
+            if ($zip->open($dphx_zip) === true) {
+                $zip->extractTo($extractPath);
+                $zip->close();
+        } else {
+            echo "Error: File not found, please contact us.";
+        }
+    
 
         
         //Steps to achieve the unzip of the dphx
@@ -71,4 +81,4 @@ class TaskController extends Controller
 
 
     }
-}
+} }
