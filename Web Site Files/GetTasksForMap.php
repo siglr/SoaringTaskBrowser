76d3dcb030fbaacc11f610aa90469bd91392b9a4
guy.larochelle <guy@siglr.com>
2024-06-13 22:14:24 -0400

    Added a map first version.

--- Web Site Files/GetTasksForMap.php
+++ Web Site Files/GetTasksForMap.php
@@ -6,15 +6,17 @@ try {
     $pdo = new PDO("sqlite:$databasePath");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Define the query to retrieve the records from WorldMapInfo
+    // Define the query to retrieve the records
     $query = "
-        SELECT EntrySeqID, 
-               TaskID, 
-               LatMin, 
-               LatMax, 
-               LongMin, 
-               LongMax
-        FROM WorldMapInfo
+        SELECT 
+            w.EntrySeqID, 
+            w.TaskID, 
+            t.Title,
+            w.PLNXML
+        FROM 
+            WorldMapInfo w
+        INNER JOIN 
+            Tasks t ON w.TaskID = t.TaskID
     ";
 
     // Prepare and execute the query
@@ -27,6 +29,7 @@ try {
     echo json_encode($worldMapInfo);
 
 } catch (PDOException $e) {
+    logMessage("Connection failed: " . $e->getMessage());
     header('Content-Type: application/json');
     echo json_encode(['error' => 'Connection failed']);
 }
