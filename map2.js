301c7c3d54185d71dc34d1eaa3990545487366f9
guy.larochelle <guy@siglr.com>
2024-07-09 14:47:24 -0400

    Removing temporary log and testing again

--- map2.js
+++ map2.js
@@ -195,15 +195,12 @@ class TaskBrowserMap {
     }
 
     highlightTask(tbm, entrySeqID) {
-        console.log('highlightTask', entrySeqID, tbm.currentEntrySeqID, tbm.currentEntrySeqID == entrySeqID);
         if (!tbm.currentEntrySeqID == entrySeqID) {
-            console.log('Really highlighting the task!');
             tbm.api_tasks[entrySeqID].polyline.setStyle({ color: '#9900cc', weight: tbm.hoverWeight });
         }
     }
 
     unhighlightTask(tbm, entrySeqID) {
-        console.log('unhighlightTask', entrySeqID, tbm.currentEntrySeqID);
         if (!tbm.currentEntrySeqID == entrySeqID) {
             tbm.api_tasks[entrySeqID].polyline.setStyle({ color: '#ff7800', weight: tbm.defWeight });
         }
