5dfa72c019c1f1ab6f1cc34f0799555910e0f49b
guy.larochelle <guy@siglr.com>
2024-07-18 16:32:32 -0400

    Updated the PHP to retrieve news to exclude excluded ones.

--- php/RetrieveNewsWSG.php
+++ php/RetrieveNewsWSG.php
@@ -7,9 +7,6 @@ try {
     $pdo = new PDO("sqlite:$newsDBPath");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Call the cleanup function
-    cleanUpNewsEntries($pdo);
-
     // Get the optional newsType parameter (default to 0 if not set or invalid)
     $newsType = isset($_GET['newsType']) ? filter_var($_GET['newsType'], FILTER_VALIDATE_INT) : 0;
 
@@ -19,7 +16,7 @@ try {
     }
 
     // Prepare and execute the query to fetch all news entries
-    $stmt = $pdo->prepare("SELECT * FROM News WHERE NewsType = :newsType ORDER BY NewsType DESC, EventDate ASC, Published DESC");
+    $stmt = $pdo->prepare("SELECT * FROM News WHERE NewsType = :newsType AND Expiration > datetime('now') ORDER BY NewsType DESC, EventDate ASC, Published DESC");
     $stmt->execute([':newsType' => $newsType]);
     $newsEntries = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
